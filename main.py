b05d87af33f467f5383bf654146ccea045eb43ce
Matthew Deacon <matthewdeacon74@gmail.com>
2024-05-29 21:22:41 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import math
+
+class Circle:
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def calculate_diameter(self):
+        return self.radius * 2
+
+    def calculate_circumference(self):
+        return self.radius * 2 * math.pi
+
+    def calculate_area(self):
+        return self.radius ** 2 * math.pi
+
+    def grow(self):
+        return self.calculate_diameter()
+
+    def get_radius(self):
+        return self.radius
+
+
+# main script
+
+valid_radius = False
+user_radius = 0
+while valid_radius == False:
+    user_radius = input(f"Please specify a radius: ")
+    try:
+        user_radius = float(user_radius)
+        valid_radius = True
+    except ValueError:
+        print("That's not a valid radius.  Numbers only (2 decimals permitted).")
+
+# Calculate circle & report results, then ask to grow or exit
+repeat = True
+while repeat == True:
+    user_circle = Circle(user_radius)
+    print(f"Your circle has diameter {user_circle.calculate_diameter()}, "
+          f"Circumference {user_circle.calculate_circumference()}, "
+          f"and Area {user_circle.calculate_area()}")
+
+    do_grow = input(f"Would you like your circle to grow? (y/n) ")
+    if do_grow == 'y':
+        print("Stand by while your circle grows...")
+        user_radius = user_circle.grow()
+    else:
+        repeat = False
+        print(f"Goodbye.  Your final circle radius was {user_circle.get_radius()}")
